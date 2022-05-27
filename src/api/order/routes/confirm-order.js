a5773fc8539dbf9cec264c6c005c5a0ad19bc4c7
Marvin Ambrose <marvambi@gmail.com>
2022-05-27 01:53:45 +0100

    feat: adds webhooks, ratelimit

--- src/api/order/routes/confirm-order.js
+++ src/api/order/routes/confirm-order.js
@@ -3,5 +3,10 @@ module.exports = {
         method: "POST",
         path: "/orders/confirm/:id",
         handler: "order.confirmOrder",
+        config: {
+            policies: ["api::order.is-owner"],
+            // attach the middleware
+            middlewares: ["api::order.ratelimit"]
+        }
     }, ]
 }
