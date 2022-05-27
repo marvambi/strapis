3a7533a0ba9b591954781644fa573d1d69018074
Marvin Ambrose <marvambi@gmail.com>
2022-05-27 01:54:41 +0100

    feat: adds webhooks, ratelimit and order owner verification

---
+++ src/api/order/policies/is-owner.js
@@ -0,0 +1,19 @@
+'use strict';
+const utils = require("@strapi/utils")
+const { PolicyError } = utils.errors
+
+module.exports = async(policyContext, config, { strapi }) => {
+
+    const { id } = policyContext.request.params
+    const user = policyContext.state.user
+    const order = await strapi.entityService.findOne("api::order.order", id, {
+        populate: ["owner"]
+    })
+
+    if (order.owner.id === user.id) {
+        // Go ahead to excecute
+        return true;
+    }
+    // throw policy error
+    throw new PolicyError("Thou shall not pass!")
+};
